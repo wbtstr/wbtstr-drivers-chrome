53d8f5a3dccf4922324269cb2d3655b208377302
Onno Valkering <ovalkering@mirabeau.nl>
2017-02-28 14:21:47 +0100

    Updated download script

--- Scripts/download-executable.ps1
+++ Scripts/download-executable.ps1
@@ -12,9 +12,6 @@ $zipChromeDriver_source = "https://chromedriver.storage.googleapis.com/{0}/{1}" 
 $zipChromeDriver_destination = Join-Path (Convert-Path ".") $zipChromeDriver_fileName
 
 # Download and unpack .zip
-$exeChromeDriver_destination = "..\\Source\\WbTstr.Drivers.Chrome"
+$exeChromeDriver_destination = "../Source/WbTstr.Drivers.Chrome"
 $wc.DownloadFile($zipChromeDriver_source, $zipChromeDriver_destination)
 [System.IO.Compression.ZipFile]::ExtractToDirectory($zipChromeDriver_destination, $exeChromeDriver_destination)
-
-Write-Host "Put Chrome Driver executable in '{0}'" -f $exeChromeDriver_destination
-sleep(5)
