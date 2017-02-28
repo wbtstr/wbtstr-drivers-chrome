886b1b8dd2c6adec5c80986c604e47c2c60180cf
Onno Valkering <ovalkering@mirabeau.nl>
2017-02-28 14:34:48 +0100

    Use 7z for .zip unpack

--- Scripts/download-executable.ps1
+++ Scripts/download-executable.ps1
@@ -1,5 +1,3 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-
 $wc = New-Object Net.WebClient
 $shell = New-Object -com Shell.Application
 
@@ -12,9 +10,7 @@ $zipChromeDriver_source = "https://chromedriver.storage.googleapis.com/{0}/{1}" 
 $zipChromeDriver_destination = Join-Path (Convert-Path ".") $zipChromeDriver_fileName
 
 # Download and unpack .zip
-$exeChromeDriver_destination = "..\\Source\\WbTstr.Drivers.Chrome"
+$exeChromeDriver_destination = "../Source/WbTstr.Drivers.Chrome"
 $wc.DownloadFile($zipChromeDriver_source, $zipChromeDriver_destination)
-[System.IO.Compression.ZipFile]::ExtractToDirectory($zipChromeDriver_destination, $exeChromeDriver_destination)
 
-Write-Host "Put Chrome Driver executable in '{0}'" -f $exeChromeDriver_destination
-sleep(5)
+7z e $zipChromeDriver_fileName -o"$exeChromeDriver_destination" -y
